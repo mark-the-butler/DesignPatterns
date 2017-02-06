1c38820bd7de769359baa3e8d35f821c08d716e9
mdb513 <mark.d.butler@sherwin.com>
2017-02-06 16:42:07 -0500

    Cleaned up tests

--- Strategy/BarbarianTests.cs
+++ Strategy/BarbarianTests.cs
@@ -5,10 +5,17 @@ namespace DesignPatterns.Strategy
     [TestFixture]
     public class BarbarianTests
     {
+        private Barbarian barbarian;
+
+        [SetUp]
+        public void Setup()
+        {
+            barbarian = new Barbarian(new MeleeStrategy());
+        }
+
         [Test]
         public void PerformAttack_Can_Do_Attack()
         {
-            var barbarian = new Barbarian(new MeleeStrategy());
             var result = barbarian.PerformAttack();
 
             Assert.That(result, Is.EqualTo("Attacked with Melee"));
@@ -17,7 +24,6 @@ namespace DesignPatterns.Strategy
         [Test]
         public void PerformAttack_Can_Change_Strategy()
         {
-            var barbarian = new Barbarian(new MeleeStrategy());
             barbarian.ChangeStrategy(new RangeAttack());
 
             var result = barbarian.PerformAttack();
