db2bb478ae2130e1daeed2cbfdc82fc320aed7b7
mdb513 <mark.d.butler@sherwin.com>
2017-03-07 08:48:23 -0500

    Create Singleton design pattern

---
+++ Singleton/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    public class Game
+    {
+        private static Game _instance;
+        private static readonly Object _lock = new Object();
+
+        public static Game instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Game();
+                        }
+                    }
+                }
+
+                return _instance ;
+            }
+        }
+
+        public Game()
+        {
+            Console.WriteLine("Building new instance!");
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Executed the game!");
+        }
+    }
+}
---
+++ Singleton/GameTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace DesignPatterns.Singleton
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void Game_Should_Always_Return_Same_Instance()
+        {
+            var instance1 = Game.instance;
+            var instance2 = Game.instance;
+
+            Assert.That(instance1, Is.EqualTo(instance2));
+        }
+    }
+}
